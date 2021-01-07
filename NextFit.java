8db67ee36f88a18bf5111db2936d12de04e90b34
dimitris <jimpaixtouras@gmail.com>
2021-01-07 18:44:25 +0200

    space

--- NextFit.java
+++ NextFit.java
@@ -12,6 +12,7 @@ public class NextFit extends MemoryAllocationAlgorithm {
         /* TODO: you need to add some code here
          * Hint: this should return the memory address where the process was
          * loaded into if the process fits. In case the process doesn't fit, it
+         *
          * should return -1. */
 
         return address;
